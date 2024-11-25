1b72ed1dc9957e100caf142f0ec516f0480fc94f
Kelp1e <vladklp22@gmail.com>
2024-11-25 18:58:03 +0100

    chore: .

--- src/pages/home/ui/start/start.tsx
+++ src/pages/home/ui/start/start.tsx
@@ -17,21 +17,31 @@ export const Start = () => {
             <ToggleMusicCheckbox />
           </div>
           <div className="flex items-center gap-[12rem]">
-            <a href={LINK.TWITTER} className="relative hover:bottom-[1rem]">
+            <a
+              href={LINK.TWITTER}
+              target="_blank"
+              className="relative hover:bottom-[1rem]"
+            >
               <Icon.Twitter className="h-[24rem]" />
             </a>
-            <a href={LINK.TELEGRAM} className="relative hover:bottom-[1rem]">
+            <a
+              href={LINK.TELEGRAM}
+              target="_blank"
+              className="relative hover:bottom-[1rem]"
+            >
               <Icon.Telegram className="h-[24rem]" />
             </a>
           </div>
         </header>
         <div className="relative flex flex-1 lg:top-[-10%] lg:w-1/3 lg:items-center">
           <div className="flex w-full flex-1 flex-col items-center justify-between gap-[40rem] lg:justify-start">
-            <img
-              src="/images/logo.png"
-              alt="hype hisher logo"
-              className="animate-flying-y relative mt-[64rem] block w-full max-w-[333rem] lg:max-w-full"
-            />
+            <a href={LINK.TWITTER} target="_blank">
+              <img
+                src="/images/logo.png"
+                alt="hype hisher logo"
+                className="animate-flying-y relative mt-[64rem] block w-full max-w-[333rem] lg:max-w-full"
+              />
+            </a>
             <StartFishingButton />
           </div>
         </div>
