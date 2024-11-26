9b6b104ce60a26e4f553de554ecd8872e8f42108
Kelp1e <vladklp22@gmail.com>
2024-11-26 13:05:17 +0100

    chore: .

--- src/pages/home/ui/news/news.tsx
+++ src/pages/home/ui/news/news.tsx
@@ -1,11 +1,14 @@
 import Marquee from "react-fast-marquee"
+import { useTranslation } from "react-i18next"
 
 export const News = () => {
+  const { t } = useTranslation()
+
   return (
     <div className="flex h-[80rem] items-center justify-center overflow-hidden bg-orange font-kemco">
       <Marquee autoFill className="flex gap-[10rem]" speed={25}>
-        <div className="mx-[20rem]">Dive into new adventures</div>
-        <div className="mx-[20rem]">Live multiplayer</div>
+        <div className="mx-[20rem]">{t("news.adventures")}</div>
+        <div className="mx-[20rem]">{t("news.multiplayer")}</div>
       </Marquee>
     </div>
   )
