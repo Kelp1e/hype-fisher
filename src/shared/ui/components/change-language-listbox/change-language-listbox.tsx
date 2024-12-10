3c2a348972565c8c1c50c73bfceb66898cf1ab57
Kelp1e <vladklp22@gmail.com>
2024-12-10 17:53:21 +0100

    chore: Waves.

--- src/shared/ui/components/change-language-listbox/change-language-listbox.tsx
+++ src/shared/ui/components/change-language-listbox/change-language-listbox.tsx
@@ -37,7 +37,7 @@ export const ChangeLanguageListbox = () => {
               key={language}
               value={language}
               className="uppercase"
-              disabled={["es", "id", "hi"].includes(language)}
+              disabled={["es", "id", "hi", "fr", "de"].includes(language)}
             >
               <div className="flex items-center gap-[8rem]">
                 <Flag country={language} className="h-[16rem]" />
